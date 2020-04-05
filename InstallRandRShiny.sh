031640e5096f2600436d9ec743edd4af268ff517
Elena Khusainova <elena.r.khusainova@gmail.com>
2020-04-05 17:28:01 -0400

    Update InstallRandRShiny.sh

--- InstallRandRShiny.sh
+++ InstallRandRShiny.sh
@@ -9,3 +9,4 @@ R CMD BATCH InstallPackages.R
 
 ## Get Shiny server
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.4.807-amd64.deb
+sudo dpkg -i shiny-server-1.4.4.807-amd64.deb
