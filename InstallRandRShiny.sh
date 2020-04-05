a096806bb342e7f5fd700ab9425632d73dd2683a
Elena Khusainova <elena.r.khusainova@gmail.com>
2020-04-05 17:25:53 -0400

    Update InstallRandRShiny.sh

--- InstallRandRShiny.sh
+++ InstallRandRShiny.sh
@@ -2,4 +2,5 @@ sudo apt-get update
 sudo apt-get install r-base
 sudo apt-get install r-base-dev
 
+wget https://raw.githubusercontent.com/elenakhusainova/EPI_Rshiny/master/InstallPackages.r
 R CMD BATCH InstallPackages.R
