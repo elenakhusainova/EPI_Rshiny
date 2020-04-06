02e561d7ee6549023ac1839a224d6c7ae08c8a58
Elena Khusainova <elena.r.khusainova@gmail.com>
2020-04-06 17:21:36 -0400

    Update InstallRandRShiny.sh

--- InstallRandRShiny.sh
+++ InstallRandRShiny.sh
@@ -3,7 +3,7 @@
 apt-get update
 echo "\n\nUpdated apt-get ...\n\n"
 
-## Get R
+# ----- Get R -----------------------
 echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list
 echo "\n\nAdded CRAN to source.list ...\n\n"
 
@@ -12,10 +12,12 @@ echo "\n\nAdded CRAN to source.list ...\n\n"
 apt-get --assume-yes install r-base r-recommended r-base-dev r-base-core # -y, --yes, --assume-yes: Automatic yes to prompts
 echo "\n\nInstalled R ...\n\n"
 
-## Get R packages I need
+# -- Get R packages I need ---------
 wget https://raw.githubusercontent.com/elenakhusainova/EPI_Rshiny/master/InstallPackages.r
 R CMD BATCH InstallPackages.R
 
-## Get Shiny server
-wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.4.807-amd64.deb
-dpkg -i shiny-server-1.4.4.807-amd64.deb
+# ----- Get Shiny server -----------
+apt-get --assume-yes install gdebi-core
+sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
+wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.2.786-amd64.deb
+gdebi shiny-server-1.4.2.786-amd64.deb
