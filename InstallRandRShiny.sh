a37e25a45d27c95b81be2538b8b72fac826eafaa
Elena Khusainova <elena.r.khusainova@gmail.com>
2020-04-06 19:34:26 -0400

    Update InstallRandRShiny.sh

--- InstallRandRShiny.sh
+++ InstallRandRShiny.sh
@@ -3,9 +3,14 @@
 apt-get update
 echo "\n\nUpdated apt-get ...\n\n"
 
+# --- Preliminary installation ------------
+apt-get --assume-yes install libxt-dev
+apt-get --assume-yes install libcairo2-dev
+echo "\n\nInstalled libxt-dev and libcairo2-dev ...\n\n"
+
 # ----- Get R -----------------------
 echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list
-echo "\n\nAdded CRAN to source.list ...\n\n"
+echo "\n\nAdded CRAN -- ubuntu xenial to source.list ...\n\n"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9 # Some magic here...
 #rversion='3.4.4-1xenial0'
 #apt-get install r-base=${rversion} r-recommended=${rversion} r-base-dev=${rversion} r-base-core=${rversion}
@@ -17,10 +22,6 @@ wget https://raw.githubusercontent.com/elenakhusainova/EPI_Rshiny/master/Install
 R CMD BATCH InstallPackages.R
 echo "\n\nInstalled R packages ...\n\n"
 
-# --- More installation ------------
-apt-get --assume-yes install libxt-dev
-apt-get --assume-yes install libcairo2-dev
-
 # ----- Get Shiny server -----------
 apt-get --assume-yes install gdebi-core
 sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
@@ -28,6 +29,11 @@ wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.2.786-am
 gdebi shiny-server-1.4.2.786-amd64.deb
 echo "\n\nInstalled R Shiny ...\n\n"
 
-# ---- Useful for debugging --------
-# R -e 'rownames(installed.packages())'
+# ---- Useful for debugging / trouble-shooting --------
+# R -e 'rownames(installed.packages())' # To get the list of installed packages
+# To upgrade R:
+# echo "deb http://www.stats.bris.ac.uk/R/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+# apt-get update
+# apt-get upgrade
 
