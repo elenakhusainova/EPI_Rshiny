91aa06e43ad01b18da5965512b7d41113f8d6d41
Elena Khusainova <elena.r.khusainova@gmail.com>
2020-04-06 17:16:33 -0400

    Update InstallRandRShiny.sh

--- InstallRandRShiny.sh
+++ InstallRandRShiny.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-sudo apt-get update
+apt-get update
+echo "\n\nUpdated apt-get ...\n\n"
 
 ## Get R
+echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list
+echo "\n\nAdded CRAN to source.list ...\n\n"
+
 #rversion='3.4.4-1xenial0'
-#sudo apt-get install r-base=${rversion} r-recommended=${rversion} r-base-dev=${rversion} r-base-core=${rversion}
-sudo apt-get install r-base r-recommended r-base-dev r-base-core
+#apt-get install r-base=${rversion} r-recommended=${rversion} r-base-dev=${rversion} r-base-core=${rversion}
+apt-get --assume-yes install r-base r-recommended r-base-dev r-base-core # -y, --yes, --assume-yes: Automatic yes to prompts
+echo "\n\nInstalled R ...\n\n"
 
 ## Get R packages I need
 wget https://raw.githubusercontent.com/elenakhusainova/EPI_Rshiny/master/InstallPackages.r
@@ -13,4 +18,4 @@ R CMD BATCH InstallPackages.R
 
 ## Get Shiny server
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.4.807-amd64.deb
-sudo dpkg -i shiny-server-1.4.4.807-amd64.deb
+dpkg -i shiny-server-1.4.4.807-amd64.deb
