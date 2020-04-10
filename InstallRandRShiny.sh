672df0ae142fcabba985f7ba1a359ee55ba11185
Elena Khusainova <elena.r.khusainova@gmail.com>
2020-04-10 17:29:16 -0400

    Update InstallRandRShiny.sh

--- InstallRandRShiny.sh
+++ InstallRandRShiny.sh
@@ -1,55 +1,62 @@
 #!/bin/sh
 
-# From Jay with some updates, worked on 04/09/2020
-debsource='deb http://cran.case.edu/bin/linux/ubuntu trusty/'
-rversion='3.2.5-1trusty0'
-# Get this and modify by hand for further package customization:
-wget http://www.stat.yale.edu/~jay/EC2/InstallPackages.R
+# ------------------------------------------------------------------------------
+# ------- Preliminary installations --------------------------------------------
+# ------------------------------------------------------------------------------
 
-echo ${debsource} >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
-
-echo "\n\nFinished update, installing R...\n\n"
-
-apt-get -y --force-yes install r-base=${rversion} r-recommended=${rversion} r-base-dev=${rversion}
-apt-get -y --force-yes install r-base-core=${rversion}
-
-wget http://www.stat.yale.edu/~jay/EC2/InstallExtras.R
-echo "\n\nFinished R, doing LaTeX and Apache...\n\n"
+echo "\n\nDoing LaTeX and Apache...\n\n"
 apt-get -y --force-yes install apache2
 apt-get -y --force-yes install libcairo2-dev
-
 echo "\n\nFinished Apache.\n\n"
+
 echo "\n\nDoing libxt, knitr, ...\n\n"
-
 apt-get -y --force-yes install libxt-dev
-R CMD BATCH InstallExtras.R        # Rserve, FastRWeb, knitr
 
-echo "\n\nDoing Shiny and FastRWeb postinstallation.\n\n"
-
-# Shiny:
 apt-get update
 apt-get -y --force-yes install python-software-properties python g++ make
 add-apt-repository ppa:chris-lea/node.js
 apt-get update
 apt-get -y --force-yes install nodejs
-sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
-apt-get install gdebi-core
 
-# From https://rstudio.com/products/shiny/download-server/ubuntu/  :
-wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.13.944-amd64.deb
-gdebi shiny-server-1.5.13.944-amd64.deb
+apt-get install gdebi-core
 
 # To repair the problem with devtools:
 # from https://github.com/r-lib/devtools/issues/2131 :
 apt install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+apt-get update
+echo "\n\nFinished update\n\n"
+
+# ------------------------------------------------------------------------------
+# ------- Installing R ---------------------------------------------------------
+# ------------------------------------------------------------------------------
+debsource='deb http://cran.case.edu/bin/linux/ubuntu trusty/'
+rversion='3.2.5-1trusty0'
+# Get this and modify by hand for further package customization:
+wget https://raw.githubusercontent.com/elenakhusainova/EPI_Rshiny/master/InstallPackages.R
+echo ${debsource} >> /etc/apt/sources.list
+
+echo "\n\nInstalling R...\n\n"
+apt-get -y --force-yes install r-base=${rversion} r-recommended=${rversion} r-base-dev=${rversion}
+apt-get -y --force-yes install r-base-core=${rversion}
+
+
+# ------------------------------------------------------------------------------
+# ------- Installing Shiny -----------------------------------------------------
+# ------------------------------------------------------------------------------
+echo "\n\nDoing Shiny.\n\n"
+# Shiny:
+sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
+# From https://rstudio.com/products/shiny/download-server/ubuntu/  :
+wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.13.944-amd64.deb
+gdebi shiny-server-1.5.13.944-amd64.deb
 
 R CMD BATCH InstallPackages.R        # bigmemory, foreach, ... also takes some time (~5min)
 
-# -------------------------------------------
-# ----- Below are some drafts.  -------------
-# -------------------------------------------
+# ------------------------------------------------------------------------------
+# ------- Below are some drafts.  ----------------------------------------------
+# ------------------------------------------------------------------------------
 
 # ---- Useful for debugging / trouble-shooting --------
 # R -e 'rownames(installed.packages())' # To get the list of installed packages
